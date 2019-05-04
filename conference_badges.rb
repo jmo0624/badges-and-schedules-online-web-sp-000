ee40d5c7c2df481f7600db5c6fe0a36bcec7d9a8
Josh Morris <jmo0624@gmail.com>
2019-05-04 21:30:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,9 @@ assign_rooms(attendees)
   room = 0
   attendees.collect do |name|
     room += 1
-    
+    "Hello #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+printer(attendees)
+  
